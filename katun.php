4340189b9dbdb9b5a879370d879c0f1927fef3ac
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-04-07 11:39:55 +0700

    Update katun.php

--- katun.php
+++ katun.php
@@ -1,12 +1,12 @@
-3.8
+3.9
 KATUN VPN
 UPDATE APP KATUN VPN
-02-04-21
+07-04-21
 Server SG 5 Server
 Server TH-CAT 1 Server
 Server TH-BM 1 Server
 Server TH-AUTO 5 Server
-Server TH-NORD 4 Server
+Server TH-NORD 5 Server
 Server LAO 1 Server
 
 SIM TRUE 
