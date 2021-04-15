82d1313cb8318a17f0dd00f02772b39d98aa4783
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-04-15 18:41:43 +0700

    Update itim.php

--- itim.php
+++ itim.php
@@ -1,9 +1,9 @@
-5.4
+5.5
 ITIM VPN
-10-04-21
+15-04-21
 SERVER
-TH-CAT 4 SERVER
-SG-VPS 2 SERVER
+TH-CAT 5 SERVER
+VIP-CAT 2 SERVER
 เซิฟ VIP-CAT ต้องเช่าครับ เดือนละ20บาท
 สนใจเช่าติดต่อทักแชทที่FB.เสี่ยเอก มงคล
 ##############################
