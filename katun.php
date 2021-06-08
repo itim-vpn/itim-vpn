12b97f5b9d2d989a359f85ff95e6a3d45f0f480d
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-06-08 10:38:50 +0700

    Update katun.php

--- katun.php
+++ katun.php
@@ -1,20 +1,23 @@
-4.8
+4.9
 KATUN VPN
-01-06-21
+08-06-21
 Server VIP-CAT 2 Server
-Server VIP-GAMING 2 Server
-Server VIP-SG 2 Server
+Server VIP-GAMING 4 Server
+Server TH-INET 1 Server
+Server SG 10 Server
 SIM TRUE 
 FB.GAMING&NOPRO
 SIM DTAC
 SOCIAL&WORK&LINE
+SIM AIS
+AIS PLAY
 BY ITIM VPN
 ####################
 -เลือกใช้ไฟล์ให้ตรงกับโปรที่สมัคร
 -กดอัพเดตไฟล์ในแอพบ่อยๆ
 -เซิฟไหนไฟล์ไหนไม่ติดให้เปลี่ยนไปใช้เซิฟอื่นไฟล์อื่นนะครับ
 -เซิฟ VIP-GAMING สำหรับใช้เล่นเกมส์เท่านั้น
--เซิฟ VIP-CAT,VIP-SG,TH-INET สำหรับใช้งานทั่วไป
+-เซิฟ VIP-CAT,SG,TH-INET สำหรับใช้งานทั่วไป
 *หมายเหตุ:ช่วยรีวิว+ให้คะแนนแอพด้วยนะครับ
 
 
