ada919d53c70aec13c3556c10ac1a0df79cf3728
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-06-22 22:28:18 +0700

    Update katun.php

--- katun.php
+++ katun.php
@@ -1,12 +1,15 @@
-5.0
+5.1
 KATUN VPN
-19-06-21
-Server VIP-CAT 2 Server
-Server VIP-GAMING 5 Server
+23-06-21
+Server TH-CAT 5 Server
+Server TH-GAMING 5 Server
 Server TH-INET 2 Server
 Server TH-RANDOM 2 Server
 Server SG 10 Server
 Server JP 5 Server
+Server LAO 1 Server
+Server VIP-CAT 2 Server
+Server VIP-GAMING 1 Server
 SIM TRUE 
 FB.GAMING&NOPRO
 SIM DTAC
@@ -19,8 +22,9 @@ BY ITIM VPN
 -กดอัพเดตไฟล์ในแอพบ่อยๆ
 -เซิฟไหนไฟล์ไหนไม่ติดให้เปลี่ยนไปใช้เซิฟอื่นไฟล์อื่นนะครับ
 -เซิฟ TH-RANDOM ต้องกดเชื่อมต่อย้ำๆถึงจะติดครับ
--เซิฟ VIP-GAMING สำหรับใช้เล่นเกมส์เท่านั้น
--เซิฟ VIP-CAT,SG,JP,TH-INET สำหรับใช้งานทั่วไป
+-เซิฟ LAO เวลาเซิฟเปิด 12:00น.-22:00น.
+-เซิฟ TH-GAMING สำหรับใช้เล่นเกมส์เท่านั้น
+-เซิฟ TH-CAT,SG,JP,TH-INET,LAO สำหรับใช้งานทั่วไป
 *หมายเหตุ:ช่วยรีวิว+ให้คะแนนแอพด้วยนะครับ
 
 
