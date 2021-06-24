c6c50e57196abfafb7b0af9c27fbbd09f73d304a
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-06-24 09:07:34 +0700

    Update katun.php

--- katun.php
+++ katun.php
@@ -1,9 +1,9 @@
-5.1
+5.2
 KATUN VPN
-23-06-21
+25-06-21
 Server TH-CAT 5 Server
 Server TH-GAMING 5 Server
-Server TH-INET 2 Server
+Server TH-INET 5 Server
 Server TH-RANDOM 2 Server
 Server SG 10 Server
 Server JP 5 Server
@@ -22,7 +22,7 @@ BY ITIM VPN
 -กดอัพเดตไฟล์ในแอพบ่อยๆ
 -เซิฟไหนไฟล์ไหนไม่ติดให้เปลี่ยนไปใช้เซิฟอื่นไฟล์อื่นนะครับ
 -เซิฟ TH-RANDOM ต้องกดเชื่อมต่อย้ำๆถึงจะติดครับ
--เซิฟ LAO เวลาเซิฟเปิด 12:00น.-22:00น.
+-เซิฟ LAO เซิฟเปิดเวลา 12:00น.-22:00น.
 -เซิฟ TH-GAMING สำหรับใช้เล่นเกมส์เท่านั้น
 -เซิฟ TH-CAT,SG,JP,TH-INET,LAO สำหรับใช้งานทั่วไป
 *หมายเหตุ:ช่วยรีวิว+ให้คะแนนแอพด้วยนะครับ
