fd15ad11f467ff17d3d18370d3845991c3288aa9
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-07-06 06:51:59 +0700

    Update katun.php

--- katun.php
+++ katun.php
@@ -1,16 +1,16 @@
-5.4
+5.5
 KATUN VPN
-30-06-21
+06-07-21
 Server TH-CAT 5 Server
 Server TH-GAMING 5 Server
 Server TH-INET 5 Server
 Server TH-RANDOM 2 Server
-Server SG 10 Server
-Server JP 5 Server
+Server TH-READY 1 Server
+Server SG-VPS 5 Server
+Server SG 15 Server
 Server LAO 1 Server
 Server VIP-CAT 2 Server
 Server VIP-GAMING 1 Server
-Server VIP-STD 1 Server
 ####################
 เซิฟ VIP-CAT เช่าเดือนละ20บาท
 เซิฟ VIP-GAMING เช่าเดือนละ30บาท
@@ -22,7 +22,8 @@ Server VIP-STD 1 Server
 -เซิฟ TH-RANDOM ต้องกดเชื่อมต่อย้ำๆถึงจะติดครับ
 -เซิฟ LAO เซิฟเปิดเวลา 12:00น.-22:00น.
 -เซิฟ TH-GAMING สำหรับใช้เล่นเกมส์เท่านั้น
--เซิฟ TH-CAT,SG,JP,TH-INET,LAO สำหรับใช้งานทั่วไป
+-เซิฟ TH-CAT,TH-INET,TH-READY,LAO,SG สำหรับใช้งานทั่วไป
+ดูหนัง ฟังเพลง
 *หมายเหตุ:ช่วยรีวิว+ให้คะแนนแอพด้วยนะครับ
 
 
