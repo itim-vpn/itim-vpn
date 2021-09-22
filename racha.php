33d34bdb40e3c38cd076f8646ad798eee4af0e3e
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-09-23 05:44:26 +0700

    Update racha.php

--- racha.php
+++ racha.php
@@ -1,19 +1,16 @@
-16.2
+16.3
          RACHA VPN
-         21-09-21
+         23-09-21
         SERVER LIST
    Server TH-CAT 10 Server
    Server TH-GAME 10 Server
    Server TH-INET 10 Server
    Server TH-READY 10 Server
    Server TH-RANDOM 1 Server
-   Server SG-AUTO 7 Server
-   Server SG-FAST 5 Server
+   Server SG-FAST 10 Server
    Server SG-RANDOM 10 Server
-   Server VIP-CAT 1 Server (Free)
-   Server VIP-FREE 1 Server (Free)
    Server VIP-CAT 4 Server
-   Server VIP-GAME 2 Server
+   Server VIP-GAME 1 Server
 
    Support Pro
    -Dtac-Line
