70fceb583365ae7c5783534e2e0c1d941c774fd7
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-10-19 02:35:09 +0700

    Update katun.php

--- katun.php
+++ katun.php
@@ -1,6 +1,6 @@
-10.7
+10.8
 KATUN VPN
-13-10-21
+19-10-21
 Server TH-CAT 10 Server
 Server TH-GAME 5 Server
 Server TH-INET 5 Server
@@ -8,22 +8,23 @@ Server TH-READY 5 Server
 Server TH-CSL 5 Server
 Server TH-RANDOM 3 Server
 Server SG-AUTO 5 Server
+Server SG-FAST 5 Server
 Server SG-RANDOM 10 Server
 Server LAO 1 Server
 Server VIP-CAT 4 Server
-Server VIP-GAME 2 Server
+Server VIP-GAME 3 Server
 Server VIP-FREE 2 Server
 Server VIP-SG 2 Server
 📢โปรดอ่านรายละเอียดก่อนใช้งาน
--เลือกใช้ไฟล์ให้ตรงกับโปรที่สมัคร
--กดอัพเดตไฟล์ทุกครั้งที่เข้าใช้งานแอพ
--เซิฟไหนไฟล์ไหนไม่ติดรึเน็ตช้าให้เปลี่ยนเซิฟเวอร์ใช้งานครับ
--เซิฟ TH-RANDOM ต้องกดเชื่อมต่อย้ำๆถึงจะติดครับ
--เซิฟ LAO เซิฟเปิดเวลา 12:00น.-22:00น.
+👉เลือกใช้ไฟล์ให้ตรงกับโปรที่สมัคร
+👉กดอัพเดตไฟล์ทุกครั้งที่เข้าใช้งานแอพ
+👉เซิฟไหนไฟล์ไหนไม่ติดรึเน็ตช้าให้เปลี่ยนเซิฟเวอร์ใช้งานครับ
+👉เซิฟ TH-RANDOM ต้องกดเชื่อมต่อย้ำๆถึงจะติดครับ
+👉เซิฟ LAO เซิฟเปิดเวลา 12:00น.-22:00น.
 📢เลือกเซิฟให้ตรงกับการใช้งาน
--เล่นเกมส์แนะนำให้ใช้เซิฟTH-GAME,TH-RANDOM
--ดูยูทูบแนะนำให้ใช้เซิฟTH-INET
--ใช้งานทั่วไปแนะนำให้ใช้เซิฟTH-CAT,TH-READY,TH-CSL,LAO,SG
+👉เล่นเกมส์แนะนำให้ใช้เซิฟTH-GAME,TH-RANDOM
+👉ดูยูทูบแนะนำให้ใช้เซิฟTH-INET
+👉ใช้งานทั่วไปแนะนำให้ใช้เซิฟTH-CAT,TH-READY,TH-CSL,LAO,SG
 👉ช่วยรีวิว+ให้คะแนนแอพด้วยนะครับ🙏
 BY ITIM VPN
 
