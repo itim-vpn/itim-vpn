fdb1cb7a3a2a1d70d27552f7bfcac6973f6182cb
itim-vpn <60966096+itim-vpn@users.noreply.github.com>
2021-11-24 08:50:16 +0700

    Update racha.php

--- racha.php
+++ racha.php
@@ -1,26 +1,17 @@
 18.3
 RACHA VPN
 UPDATE 24-11-21
-TH-CAT 10 SERVER
-TH-GAME 10 SERVER
-TH-INET 10 SERVER
-TH-READY 10 SERVER
-TH-RANDOM 3 SERVER
-SG-AUTO 10 SERVER
-SG-FAST 5 SERVER
-VIP-CAT 4 SERVER
-VIP-CSL 2 SERVER
-VIP-GAME 3 SERVER
-VIP-FREE 2 SERVER
-VIP-SG 2 SERVER
+เพิ่มเซิฟ FAST 5 SERVER
+เพิ่มเซิฟ SG 5 SERVER
+เพิ่มเซิฟ GAME 5 SERVER
 📢โปรดอ่านรายละเอียดก่อนใช้งาน
 ✅เลือกใช้ไฟล์ให้ตรงกับโปรที่สมัคร
 ✅กดอัพเดตไฟล์ในแอพทุกวัน
 ✅เซิฟไหนไฟล์ไหนไม่ติดรึเน็ตช้าให้เปลี่ยนไฟล์ในการใช้งาน
 📢เลือกเซิฟเวอร์ให้ตรงกับการใช้งาน
-✅เล่นเกมส์ให้ใช้เซิฟTH-GAME
-✅ดูยูทูบให้ใช้เซิฟTH-INET
-✅ใช้งานทั่วไปให้ใช้เซิฟTH-CAT,TH-READY,SG
+✅เล่นเกมส์ให้ใช้เซิฟ GAME
+✅ดูยูทูบให้ใช้เซิฟ INET
+✅ใช้งานทั่วไปให้ใช้เซิฟ CAT,READY,SG,FAST
 👉ช่วยรีวิว+ให้คะแนนแอพให้ด้วยนะครับ🙏
 👉ช่วยกดดูโฆษณาด้วยนะครับ🙏
 BY ITIM VPN
